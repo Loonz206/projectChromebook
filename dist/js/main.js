3752ba1253ea20b3b7ff6fe03055f03ebfe30ab3
Lenny Peters <mr.lennypeters@gmail.com>
2015-07-23 17:48:06 -0700

    Master - Added some silly stuff and I will probably remove it later

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,25 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController', function MyController($scope) {
+  $scope.playerList = [
+  {
+    'name' : 'Russell Wilson',
+    'position' : 'Quarterback',
+    'team' : 'Seattle Seahawks',
+    'city' : 'Seattle',
+    'state' : 'Washington',
+    'positionNumber' : 3
+  },
+  {
+    'name' : 'Marshawn Lynch',
+    'position' : 'Runningback',
+    'team' : 'Seattle Seahawks',
+    'city' : 'Seattle',
+    'state' : 'Washington',
+    'positionNumber' : 24
+  }
+  ];
+});
 
 // Avoid `console` errors in browsers that lack a console.
 (function() {
