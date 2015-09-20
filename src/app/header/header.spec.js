2b4c7790eceba51ac65542d78bbdca93faf0e425
Lenny Peters <mr.lennypeters@gmail.com>
2015-09-19 17:02:57 -0700

    master - added vendor folder and local fallbacks for issues with CDN calls

---
+++ src/app/header/header.spec.js
@@ -0,0 +1,29 @@
+describe('sometest', function(){
+	it('should be true', function(){
+		expect(true).toEqual(true);
+	});
+});
+
+describe('an addition test', function (){
+	var a,b;
+	beforeEach(function(){
+		a = 2;
+		b = 1;
+	});
+	it('should come to the value of three', function(){
+		var result = a + b;
+		expect(result).toEqual(3);
+	});
+	it('should not to come to the value of 13', function (){
+		a = 12;
+		var result = a + b;
+		expect(result).not.toEqual(3);
+	});
+});
+
+describe('another silly test', function (){
+	it('should be false', function (){
+		expect(false).toEqual(false);
+	});
+});
+
