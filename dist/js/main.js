17fddc9a5da74d023438912875e3026a600029ef
Lenny Peters <mr.lennypeters@gmail.com>
2015-09-26 17:48:51 -0700

    Master - installed some states after figuring out the folder structure was the pain point

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,41 +1,18 @@
-angular.module('projectChromebook', [
-    'ui.router'
+angular.module('myApp', [
+  'ui.router'
 ])
-    .config(function($stateProvider, $urlRouterProvider){
-        //
-        // For any unmatched url, redirect to /state1
-        $urlRouterProvider.otherwise('/state1');
-        //
-        // Now set up the
-            $stateProvider
-            .state('state1', {
-              url: "/state1",
-              templateUrl: "partials/state1.html"
-            })
-            .state('state1.list', {
-              url: "/list",
-              templateUrl: "partials/state1.list.html",
-              controller: function($scope) {
-                $scope.items = ["A", "List", "Of", "Items"];
-              }
-            })
-            .state('state2', {
-              url: "/state2",
-              templateUrl: "partials/state2.html"
-            })
-            .state('state2.list', {
-              url: "/list",
-              templateUrl: "partials/state2.list.html",
-              controller: function($scope) {
-                $scope.things = ["A", "Set", "Of", "Things"];
-              }
-            });
-    })
 
-    .controller('MainController', function (){
-        //stuff goes here
-    });
-
+.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home/home.html'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'about/about.html'
+      })
+}])
 describe('sometest', function(){
 	it('should be true', function(){
 		expect(true).toEqual(true);
@@ -80,3 +57,5 @@ describe('', function (){
 		expect(a + b).not.toEqual(13);
 	});
 });
+
+
