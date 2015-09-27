12558c35eb7e0c372effd9a4cf00a5a88bcd5d2d
Lenny Peters <mr.lennypeters@gmail.com>
2015-09-27 10:48:20 -0700

    Master - templates now moved into app folder

---
+++ src/app/app.js
@@ -0,0 +1,39 @@
+angular.module('myApp', [
+  'ui.router'
+])
+
+.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/home/home.html',
+        data: {
+          pageTitle: 'Home'
+        }
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+        data: {
+          pageTitle: 'About'
+        }
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'app/contact/contact.html',
+        data: {
+          pageTitle: 'Contact'
+        }
+      });
+}])
+
+.controller( 'AppCtrl', function AppCtrl ( $scope ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' || ProjectChromebook' ;
+    }
+  });
+});
