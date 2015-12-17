c38a4b4b16499595b3f3f7ac12b13fed4802d735
Lenny Peters <lpeters@Lenny-Peters-MacBook-Pro.local>
2015-12-16 16:39:33 -0800

    Master - Phantom JS as the headless browser testing tool

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,10 @@
+//karma.conf.js
 module.exports = function(config) {
   config.set({
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     frameworks: ['jasmine'],
     files: [
+      'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.js',
       'src/app/**/*.spec.js',
       'src/app/*.js'
     ],
