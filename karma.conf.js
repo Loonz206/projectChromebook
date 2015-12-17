a768f58507c4ea890959199c3877a25811200838
Lenny Peters <lpeters@Lenny-Peters-MacBook-Pro.local>
2015-12-16 16:41:11 -0800

    Master - Comments

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config) {
     browsers: ['PhantomJS'],
     frameworks: ['jasmine'],
     files: [
+    //Load the scripts and or files needed to run the tests
       'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.js',
       'src/app/**/*.spec.js',
       'src/app/*.js'
